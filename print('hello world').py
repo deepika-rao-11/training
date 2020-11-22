cd45d7789b6598a4ea02bd1556e9ff7ff9fc532a
deepika-rao-11 <74701660+deepika-rao-11@users.noreply.github.com>
2020-11-22 19:06:51 +0530

    Add files via upload

---
+++ print('hello world').py
@@ -0,0 +1,7 @@
+A=input("enter the value of A: ")
+B=input("enter the value of B: ")
+print("The value of A is "+A)
+print("The value of B is "+B)
+
+print("The multiplication of A and B is", int(A) * int(B))
+  
